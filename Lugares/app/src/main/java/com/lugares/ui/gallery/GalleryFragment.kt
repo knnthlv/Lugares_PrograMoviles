f6bfa939989cde673fa5da538605c2a8ac7740be
Knnthlv <kleiva730944@ufide.ac.cr>
2022-04-07 00:03:56 -0600

    Commit semana 11

--- Lugares/app/src/main/java/com/lugares/ui/gallery/GalleryFragment.kt
+++ Lugares/app/src/main/java/com/lugares/ui/gallery/GalleryFragment.kt
@@ -1,16 +1,29 @@
 package com.lugares.ui.gallery
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lugares.databinding.FragmentGalleryBinding
+import com.lugares.model.Lugar
 import com.lugares.viewmodel.GalleryViewModel
+import com.lugares.viewmodel.LugarViewModel
 
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentGalleryBinding? = null
 
@@ -18,7 +31,67 @@ class GalleryFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
+    private lateinit var googleMap: GoogleMap
+    private var mapReady = false
+
+    private lateinit var lugarViewModel: LugarViewModel
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //Se solicita la actualizacion del mapa.. despliegue en pantalla
+        binding.map.onCreate(savedInstanceState)
+        binding.map.onResume()
+        binding.map.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap){
+        map.let{
+            googleMap = it
+            mapReady = true
+
+            //Se instruye al mapa para que se actualicen las ubicaciones según el view model
+            lugarViewModel.getAllData.observe(viewLifecycleOwner){lugares ->
+                updateMap(lugares)
+                ubicaGPS()
+            }
+        }
+    }
+
+    private fun updateMap(lugares: List<Lugar>){
+        if(mapReady){
+            lugares.forEach{lugar ->
+                if (lugar.latitud?.isFinite()==true && lugar.longitud?.isFinite() ==true){
+                val marker = LatLng(lugar.latitud,lugar.longitud)
+                googleMap.addMarker(MarkerOptions().position(marker).title(lugar.nombre))
+
+                }
+            }
+        }
+    }
+
+    private fun ubicaGPS(){
+        val fusedLocation: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+        if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION),105)
+        }
+        fusedLocation.lastLocation.addOnSuccessListener { location: Location? ->
+            if(location != null){
+                val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                    LatLng(9.97,-84.00/*location.latitude,location.longitude*/),15f)
+                googleMap.animateCamera(cameraUpdate)
+            }
+        }
+
+    }
+
+    /*override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,6 +107,18 @@ class GalleryFragment : Fragment() {
             textView.text = it
         }
         return root
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+
+        return binding.root
     }
 
     override fun onDestroyView() {
